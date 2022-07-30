d934499aef963f5c921daa30d652b1003df75847
GolikovMatwey <nicematwey@yandex.ru>
2022-07-30 19:13:57 +0300

    commit homework number

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,30 @@
+﻿ Console.WriteLine ("Введите 3 числа");
+ string? Console.ReadLine();
+ string? Console.ReadLine();
+ string? Console.ReadLine();
+
+int iuntA = int.Parse(A);
+int iuntB = int.Parse(B);
+int iuntC = int.Parse(C);
+
+if (iuntA > iuntB)
+{ 
+    if (iuntA > iuntC)
+    {
+    Console.WriteLine(iuntA);
+    }
+    else
+    {
+    Console.WriteLine(iuntC);
+    }
+} 
+else
+{ if (iuntB > iuntC)
+{
+    Console.WriteLine(iuntB);
+}
+else
+{
+    Console.WriteLine(iuntC);
+}   
+}
